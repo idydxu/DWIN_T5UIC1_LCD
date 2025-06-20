2998d0ac12828534d60509d39aeba127fd8b32f1
idydxu <55757107+idydxu@users.noreply.github.com>
2025-06-19 20:24:38 -0500

    Update run.py
    passthrough user from bash cmd / 
    local run

--- run.py
+++ run.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
+import sys
+import os
 from dwinlcd import DWIN_LCD
+
+#pass user through from bash command or local user if running manually
+if len(sys.argv)>1:
+    user = str(sys.argv[1])
+else:
+    user = str(os.getlogin())
 
 # Change pins if you do not wire to the ReadMe
 encoder_Pins = (26, 19) # For Ender3 v2
 #encoder_Pins = (19,26) # For Other Displays, inverts dial encoder
+
 button_Pin = 13
 LCD_COM_Port = '/dev/ttyAMA0'
-API_Key = 'XXXXXX' #API key for Moonraker/Moonsail can be found at http://<klipper host>/access/api_key
+API_Key = 'd22a79f1a2c745a29ed0e528741f92f3'
 
 DWINLCD = DWIN_LCD(
 	LCD_COM_Port,
 	encoder_Pins,
 	button_Pin,
-	API_Key
+	API_Key,
+    user
 )
