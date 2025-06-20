d7b919d0463cd4e66a82feb9b004ecb42ac46f17
idydxu <55757107+idydxu@users.noreply.github.com>
2025-06-19 22:34:26 -0500

    Update run.py

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ encoder_Pins = (26, 19) # For Ender3 v2
 
 button_Pin = 13
 LCD_COM_Port = '/dev/ttyAMA0'
-API_Key = 'd22a79f1a2c745a29ed0e528741f92f3'
+API_Key = 'XXXXXX'
 
 DWINLCD = DWIN_LCD(
 	LCD_COM_Port,
